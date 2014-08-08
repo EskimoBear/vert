aa013a6bec47e6f15d4a9e9613bb73fa7bf59be0
Andre Dickson <andre@mobilitti.com>
2014-08-08 06:38:00 -0400

    Completed validation and verification checks
    - Added JSON validation checks
    - Added Avro based verification checks

--- lib/vert.rb
+++ lib/vert.rb
@@ -1,14 +1,28 @@
+require 'oj'
+require 'avro'
+
 module Vert
 
   extend self
 
   ValidationError = Class.new(StandardError)
+  VerificationError = Class.new(StandardError)
 
+  #error messages for hash
   NOT_A_HASH_ERROR = "Not a hash." 
   EMPTY_ERROR = "The hash is empty." 
-  ABSENT_KEY_ERROR = "The hash does not contain the following key/s"  
-  HASH_KEY_ERROR = "The hash map either does not contain key/s provided or the keys are present but do not have hash map as its value."
+  ABSENT_KEY_ERROR = "The data does not contain the following key/s"  
 
+  #error messages for json
+  EMPTY_JSON_ERROR = "The JSON string is empty."
+  EMPTY_JSON_OBJECT_ERROR = "The JSON object is empty."
+  MALFORMED_JSON_ERROR = "The JSON string is malformed."
+  ABSENT_JSON_ERROR = "JSON keys absent."
+
+  #error messages for avro
+  INVALID_AVRO_SCHEMA_ERROR = "The avro schema is invalid."
+  INVALID_AVRO_DATUM_ERROR = "The hash provided is not an instance of the schema:"
+  
   TYPE_ENUM = {array_keys: Array, hash_keys: Hash}
 
   def validate(hash, keys = {}) 
@@ -23,6 +37,8 @@ module Vert
     end
   rescue ValidationError => exception 
     build_error_output(exception) 
+  else
+    nil
   end
 
   def raise_when_empty(hash) 
@@ -70,8 +86,51 @@ module Vert
     {errors: [{type: custom_exception.class.to_s, message: custom_exception.message}]}
   end
 
-  def validated?(hash, keys = {})
-    validate(hash, keys).nil? ? true: false 
+  def validate?(hash, keys = {})
+    validate(hash, keys).nil? ? true : false 
+  end
+
+  def validate_json(json, keys = {})
+    hash = parse_json(json)
+  rescue ValidationError => exception
+    build_error_output(exception)
+  else
+    validate(hash, keys)
+  end
+
+  def validate_json?(json, keys = {})
+    validate_json(json, keys).nil? ? true : false 
+  end
+
+  def parse_json(json)
+    hash = Oj.load(json)
+    raise ValidationError, EMPTY_JSON_ERROR if hash.nil?
+    raise ValidationError, EMPTY_JSON_OBJECT_ERROR if hash.empty?
+  rescue Oj::ParseError => exception
+    raise ValidationError, "#{MALFORMED_JSON_ERROR} #{exception.message.gsub( /\[.+\]/, "").rstrip}."
+  else
+    hash
+  end
+
+  def verify(data, schema)
+    schema_object = parse_avro_schema(schema)
+    raise VerificationError, INVALID_AVRO_DATUM_ERROR unless Avro::Schema.validate(schema_object, data)
+  rescue VerificationError => exception
+    build_error_output(exception)
+  else
+    nil
+  end
+
+  def parse_avro_schema(schema_string)
+    schema = Avro::Schema.parse(schema_string)
+  rescue Avro::SchemaParseError => exception
+    raise VerificationError, "#{INVALID_AVRO_SCHEMA_ERROR} #{exception}"
+  else
+    schema
+  end
+
+  def verify?(data, schema)
+    verify(data, schema).nil? ? true : false 
   end
 
 end
